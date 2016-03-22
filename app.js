4ff886885bc15f04a65d639e91c52c79f7835515
Bogdan Raducan <bogdan.raducan@emag.ro>
2016-03-22 13:34:53 +0200

    User foursquare service to get the most popular vene from a location coordinates

--- app.js
+++ app.js
@@ -2,26 +2,33 @@ var locationName = 'Munchen';
 
 var coordinatesService = require('./coordinatesService.js');
 var flickrService = require('./flickrService.js');
+var foursquareService = require('./foursquareService.js');
 
 var locationData = {};
 
-coordinatesService.getCoordinates(locationName, function(coordinates){
-  console.log(coordinates);
+coordinatesService.getCoordinates(locationName,
+  function(coordinates){
+    console.log("Coordinates of " + locationName + ": " + coordinates);
 
-  var lat = coordinates[0];
-  var lon = coordinates[1];
+    var lat = coordinates[0];
+    var lon = coordinates[1];
 
-  var locationCoordinates = {};
-  locationCoordinates.lat = lat;
-  locationCoordinates.lon = lon;
+    var locationCoordinates = {};
+    locationCoordinates.lat = parseFloat(lat).toFixed(3);
+    locationCoordinates.lon = parseFloat(lon).toFixed(3);
 
-  locationData.coordinates = locationCoordinates;
+    locationData.coordinates = locationCoordinates;
 
-  if (!locationData.weather) { return; }
+    foursquareService.getNameForMostPopularVenue(locationCoordinates,
+      function(venueName){
+        console.log("Most popular venue in " + locationName + ": " + venueName);
 
-  getPhotos(locationData);
-});
+        locationData.mostPopularVenueName = venueName;
 
+        getPhotosForLocationData(locationData);
+      });
+    }
+  );
 
 
 var getPhotos = function(locationData) {
